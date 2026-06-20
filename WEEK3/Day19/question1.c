7a2a0f8a55dc197e98ffa9cba4250d4d533900f7
omnarayanmishra07creates <omnarayanmishra07@gmail.com>
2026-06-20 22:24:31 +0530

    Add files via upload

---
+++ WEEK3/Day19/question1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], b[10][10], c[10][10];
+    int i, j, row, col;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d%d", &row, &col);
+
+    printf("Enter first matrix:\n");
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Adding matrices
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum of matrices:\n");
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ WEEK3/Day19/question3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int i, j, row, col;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &row, &col);
+
+    printf("Enter matrix elements:\n");
+
+    for(i=0; i<row; i++)
+    {
+        for(j=0; j<col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Transpose matrix is:\n");
+
+    // Printing transpose
+    for(i=0; i<col; i++)
+    {
+        for(j=0; j<row; j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
